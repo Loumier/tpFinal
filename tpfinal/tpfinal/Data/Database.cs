f91a72783f52811da147668dc9073ac9e207f91b
Loumier <loumier28@gmail.com>
2019-12-06 20:27:29 -0200

    Versão funcionando o banco de dados

---
+++ tpfinal/tpfinal/Data/Database.cs
@@ -0,0 +1,116 @@
+﻿
+using Android.Util;
+using App.Crud_Xamarin.Resources.Model;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+namespace App.Crud_Xamarin.Resources.DataBaseHelper
+{
+    public class DataBase
+    {
+        string pasta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        public bool createDatabase()
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    conexao.CreateTable<Library>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool insertBook(Book book)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    conexao.Insert(book);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public List<Book> GetBooks()
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    return conexao.Table<Book>().ToList();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+
+        public bool updateBook(Book book)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    conexao.Query<Book>("UPDATE Library set Nome=?,Autor=?, Email=?, Isbn=? Where Id=?", book.Name, book.Author, book.email, book.isbn);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool deleteBook(Aluno aluno)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    conexao.Delete(book);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool getBook(int Id)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(System.IO.Path.Combine(pasta, "Books.db")))
+                {
+                    conexao.Query<Aluno>("SELECT * FROM Library Where Id=?", Id);
+                    //conexao.Update(aluno);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
--- tpfinal/tpfinal/Model/Book.cs
+++ tpfinal/tpfinal/Model/Book.cs
@@ -9,28 +9,20 @@ namespace tpfinal.Model
 {
     class getConnection
     {
-        //database = DependencyService.Get<ISQLite>().getConnection();
-        //database.CreateTable<books>();
-
         #region Propriedades
-        //[PrimaryKey, AutoIncrement]
+        [PrimaryKey, AutoIncrement]
         private int id;
-        private String Name;
-        private string Author;
-        private string email;
-        private string isbn;
+        private String Name { get; set; }
+        private string Author { get; set; }
+        private string email { get; set; }
+        private string isbn { get; set; }
         #endregion
 
-        public string Name1 { get => Name; set => Name = value; }
-        public string Author1 { get => Author; set => Author = value; }
-        public string Email { get => email; set => email = value; }
-        public string Isbn { get => isbn; set => isbn = value; }
-        public int Id { get => id; set => id = value; }
-
-        #region book Local Database
-        private SQLiteConnection database;
-        static object locker = new object();
-        #endregion
+        //public string Name { get => Name; set => Name = value; }
+        //public string Author { get => Author; set => Author = value; }
+        //public string Email { get => email; set => email = value; }
+        //public string Isbn { get => isbn; set => isbn = value; }
+        //public int Id { get => id; set => id = value; }
 
     }
 }
